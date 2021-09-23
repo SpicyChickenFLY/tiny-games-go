fe3860c58eb09a7ab7237af460fc5e62cbb933dd
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-09-23 18:32:25 +0800

    update

--- lib/gameTetris/core.go
+++ lib/gameTetris/core.go
@@ -1,136 +1,291 @@
 package gameTetris
 
-const (
-	emptyTile = 0
+import (
+	"math"
+	"math/rand"
+	"time"
 )
 
-// operations
+const (
+	emptyTile = 0
+	tetriNum  = 4
+)
+
+// operation type (Chapter 4)
 const (
 	moveLeft = iota
 	moveRight
-	rotate
-	accelerate
-	decelerate
+	softDrop
+	hardDrop
+	rotateClockwise
+	rotateCounterClockwise
+	hold
 )
 
-var shapeLib = []int{
-	// Two-status-shape
-	0xC840, 0x3210, // I-Shape
-	0x5410, 0x5410, // O-shape
-	0x6510, 0x8541, // filped-Z-shape
-	0x5421, 0x9540, // Z-shape
-	// Four-status-shape
-	0x6541, 0x9651, 0x9654, 0x9541, // T-shape
-	0x6542, 0xA951, 0x8654, 0x9510, // 7-shape
-	0x6540, 0x9521, 0xA654, 0x9851, // filped-7-shape
+// Tetrimino facings (A 1.3)
+//   _____________
+//   | 0| 1| 2| 3|
+//   | 4| 5| 6| 7|
+//   | 8| 9| A| B|
+//   | C| D| E| F|
+//   -------------
+var tetriminoShapes = [][]int{
+	{0x6521, 0x6521, 0x6521, 0x6521}, // O-tetrimino
+	{0x7654, 0xEA62, 0xBA98, 0xD951}, // I-tetrimino
+	{0x6541, 0x9651, 0x9654, 0x9541}, // T-tetrimino
+	{0x6542, 0xA951, 0x8654, 0x9510}, // L-tetrimino
+	{0x6540, 0x9521, 0xA654, 0x9851}, // J-tetrimino
+	{0x5421, 0xA651, 0x9865, 0x9540}, // S-tetrimino
+	{0x6510, 0x9652, 0xA954, 0x8541}, // Z-tetrimino
 }
-
-var shapeColor = []int{}
 
 // Game implement Game interface
 type Game struct {
-	board                               []int
-	height, width, boardSize            int
-	shapeX, shapeY, shapeSize, shapeIdx int
-	score, difficult, delay             int
-	alive                               bool
+	playfield                                     []int
+	height, bufferHeight, width                   int
+	stash                                         []int
+	bag, nextBag                                  []int
+	bagIdx                                        int
+	tetriminoX, tetriminoY, tetriminoSize         int
+	tetriminoIdx, tetriminoDrct, nextTetriminoIdx int
+	ghostX, ghostY                                int
+	level                                         int
+	fallSpeed                                     float64
+	lockDelay                                     int
+	score, difficult, lockDownDelay               int
+	alive                                         bool
+	hardDropFlag, softDropFlag                    bool
+	movedFlag, spaceFlag, landFlag                bool
+	patternMatchFlag                              bool
+	allowSRS, allowGhost, allowHardDropOp         bool
+	inputCh                                       chan int
 }
 
 // ============== Main Progress ================
 
-// if we assum width=4, height=4
-// the g.board is a 1-demension slice like
-//  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-// but we can regard it as follow
-//  [
-//    0,  1,  2,  3,
-//    4,  5,  6,  7,
-//    8,  9, 10, 11,
-//    12, 13, 14, 15
-//  ]
-// and we can judge the index by following equation:
-//  index = width * row_index + column_index
-//  e.g. g.board[2][1] = 4 * 2 + 1 = 9 = g.board[9]
-//
-// so the 4 direction can be defined as integer
-//  UP: 	width * -1
-//  DOWN: 	width
-//  LEFT: 	-1
-//  RIGHT:	1
 func (g *Game) init(w, h, difficult int) {
+	rand.Seed(time.Now().UnixNano())
 	g.score = 0
-	g.fps = 60
 	g.difficult = difficult
-	g.delay = difficult
-	g.width, g.height, g.boardSize = w, h, w*h
-	g.board = make([]int, g.width*g.height)
-}
-
-func (g *Game) removeShapeFromBoard() {
-	for pos := shapeLib[g.shapeIdx]; pos > 0; pos >>= 4 {
-		_, _, pob := g.calPosOnBoard(pos)
-		g.board[pob] = emptyTile
-	}
-}
-
-func (g *Game) addShapeToBoard() {
-	color := shapeColor[g.shapeIdx]
-	for pos := shapeLib[g.shapeIdx]; pos > 0; pos >>= 4 {
-		_, _, pob := g.calPosOnBoard(pos)
-		g.board[pob] = color
-	}
-}
-
-func (g *Game) calPosOnBoard(posOnShape int) (x, y, pos int) {
-	x = g.shapeX + posOnShape%g.shapeSize
-	y = g.shapeY + posOnShape/g.shapeSize%g.shapeSize
-	pos = y*g.width + x
-	return x, y, pos
-}
-
-func (g *Game) rotate() {
+	g.level = difficult
+	g.width, g.height, g.bufferHeight = w, h, h
+	g.playfield = make([]int, g.width*(g.height+g.bufferHeight))
+	g.calcFallSpeed()
 
 }
 
-func (g *Game) move(isDirectionLeft bool) {
-	g.removeShapeFromBoard()
+func (g *Game) calcPosOnBoard(posOnShape int) (x, y int) {
+	x = g.tetriminoX + posOnShape%tetriNum
+	y = g.tetriminoY - posOnShape/tetriNum%tetriNum
+	return x, y
+}
 
-	if isDirectionLeft {
-		pos
+func (g *Game) move(isDrctLeft bool) {
+	if isDrctLeft {
+		g.tetriminoX--
 	} else {
-
+		g.tetriminoX++
 	}
-	for pos := shapeLib[g.shapeIdx]; pos > 0; pos >>= 4 {
-		x, _, pob := g.calPosOnBoard(pos)
-		if x < 0 || x >= g.width || g.board[pob] != 0 {
+	for i := tetriminoShapes[g.tetriminoIdx][g.tetriminoDrct]; i != 0; i >>= 4 {
+		x, y := g.calcPosOnBoard(i)
+		if x < 0 || x >= g.width || g.playfield[x+y*g.width] != 0 {
+			if isDrctLeft {
+				g.tetriminoX++
+			} else {
+				g.tetriminoX--
+			}
 			return
 		}
 	}
-	g.addShapeToBoard()
 }
 
-func (g *Game) drop() {
+func (g *Game) rotate(isClockwise bool) {
+	if g.allowSRS {
+		g.superRotate(isClockwise)
+	} else {
+		g.classiscRotate(isClockwise)
+	}
+}
+
+func (g *Game) classiscRotate(isClockwise bool) {}
+
+func (g *Game) superRotate(isClockwise bool) {
 
 }
 
-func (g *Game) accelerate() {
-
+func (g *Game) checkLanded() bool {
+	g.tetriminoY--
+	for i := tetriminoShapes[g.tetriminoIdx][g.tetriminoDrct]; i != 0; i >>= 4 {
+		x, y := g.calcPosOnBoard(i)
+		if g.playfield[x+y*g.width] != 0 {
+			g.tetriminoY++
+			return false
+		}
+	}
+	return true
 }
 
-func (g *Game) operate(operation int) {
-	switch operation {
-	case moveLeft:
-		g.move(true)
-	case moveRight:
-		g.move(false)
-	case rotate:
-		g.rotate()
-	case accelerate:
-		g.accelerate()
-	case decelerate:
-		g.decelerate()
+func (g *Game) softDrop() {
+	if g.softDropFlag {
+		g.calcFallSpeed()
+	} else {
+		g.calcDropSpeed()
+	}
+}
+
+func (g *Game) hardDrop() {
+	g.tetriminoX, g.tetriminoY = g.ghostX, g.ghostY
+	g.hardDropFlag = true
+}
+
+func (g *Game) accelerate() {}
+
+// get from bag system (A 1.2.1)
+func (g *Game) genFromBag() {
+	if g.bagIdx == len(g.bag) {
+		for i := 0; i < len(tetriminoShapes); i++ {
+			g.bag = append(g.bag, i)
+		}
+		rand.Shuffle(
+			len(g.bag),
+			func(i, j int) {
+				g.bag[i], g.bag[j] = g.bag[j], g.bag[i]
+			})
+		g.bagIdx = 0
+	}
+	g.tetriminoIdx = g.bag[g.bagIdx]
+	if g.bagIdx == len(g.bag)-1 {
+		g.nextTetriminoIdx = tetriminoShapes[g.nextBag[0]][0]
+	} else {
+		g.nextTetriminoIdx = tetriminoShapes[g.bag[g.bagIdx+1]][0]
+	}
+}
+
+// generration Phase (A 1.2.1)
+func (g *Game) generationPhase() {
+	// Random Generation
+	g.genFromBag()
+	// Generation of Tetriminos
+	// TODO: delay time?
+	// Starting Location and Orirntation
+	g.tetriminoX = (g.width - tetriNum) / 2
+	g.tetriminoY = g.height + 1
+}
+
+func (g *Game) processInput() {
+	for input := range g.inputCh {
+		switch input {
+		case moveLeft:
+			g.move(true)
+		case moveRight:
+			g.move(false)
+		case rotateClockwise:
+			g.rotate(true)
+		case rotateCounterClockwise:
+			g.rotate(false)
+		case softDrop:
+			g.softDrop()
+		case hardDrop:
+			g.hardDropFlag = true
+		}
+	}
+}
+
+func (g *Game) fallingPhase() {
+	if !g.checkLanded() {
+		startTime, endTime := time.Now(), time.Now()
+		for endTime.Sub(startTime) < time.Duration(g.fallSpeed)*time.Millisecond {
+			g.processInput()
+			if g.hardDropFlag && !g.allowHardDropOp {
+				return
+			}
+		}
 	}
 
-	BadExpr
+	g.tetriminoY--
 
+	g.lockPhase()
+}
+
+func (g *Game) lockPhase() {
+	startTime, endTime := time.Now(), time.Now()
+	for endTime.Sub(startTime) < time.Duration(g.lockDownDelay)*time.Millisecond {
+		g.processInput()
+		if g.hardDropFlag && !g.allowHardDropOp {
+			return
+		}
+	}
+	if g.movedFlag {
+		if g.spaceFlag {
+			g.fallingPhase()
+		} else if g.landFlag {
+			g.lockPhase()
+		}
+	}
+}
+
+func (g *Game) patternPhase() {
+	if g.patternMatchFlag {
+		// Mark Block for Destruction
+	}
+	g.iteratePhase()
+}
+
+func (g *Game) iteratePhase() {
+
+}
+
+func (g *Game) animatePhase() {}
+
+func (g *Game) detectLineClear() bool {
+	return false // FIXEME: not done
+}
+
+func (g *Game) elimatePhase() {
+	if g.detectLineClear() {
+		g.elimatePhase()
+	}
+	g.completionPhase()
+}
+
+func (g *Game) completionPhase() {}
+
+// calculate the fall speed in current level (unit: Millisecond Per Line)
+func (g *Game) calcFallSpeed() {
+	g.fallSpeed = math.Pow(0.8-float64(g.level-1)*0.007, float64(g.level-1)) * 1000
+}
+
+func (g *Game) calcDropSpeed() {
+	g.calcFallSpeed()
+	g.fallSpeed = g.fallSpeed / 20
+}
+
+// Run the game
+func (g *Game) Run() {
+	// Tetris engine flowchart
+	for true {
+		g.generationPhase()
+		for true {
+			g.fallingPhase()
+			if g.hardDropFlag && !g.allowHardDropOp {
+				break
+			}
+			for g.movedFlag && !g.spaceFlag && g.landFlag {
+				g.lockPhase()
+			}
+			if !g.movedFlag || (g.movedFlag && !g.spaceFlag && !g.landFlag) {
+				break
+			}
+		}
+		g.patternPhase()
+		if g.patternMatchFlag {
+			// Mark block for destruction
+		}
+		g.iteratePhase()
+		g.animatePhase()
+		for g.detectLineClear() {
+			g.elimatePhase()
+		}
+		g.completionPhase()
+	}
 }
---
+++ lib/gameTetris/interface.go
@@ -0,0 +1,64 @@
+package gameTetris
+
+import (
+	"fmt"
+
+	"github.com/nsf/termbox-go"
+)
+
+const (
+	keyEsc = 0
+
+	keyUp    = 1
+	keyDown  = 2
+	keyLeft  = 4
+	keyRight = 8
+)
+
+func renderFrame(
+	g Game,
+	renderFunc func(board []int, height, width, score, fps int),
+	stopCh <-chan struct{}) {
+
+	for {
+		select {
+		case <-stopCh:
+			return
+		default:
+			renderFunc(g.board, g.height, g.width, g.score, g.fps)
+		}
+	}
+}
+
+func run(
+	g Game,
+	width, height, difficult int,
+	inputChannel chan int,
+	renderFunc func(board []int, height, width, score, fps int),
+) (score int) {
+	g.init(4, 4, 2)
+
+	stopRenderCh := make(chan struct{})
+	go render(g, renderFunc, stopRenderCh)
+	defer close(stopRenderCh)
+
+	process(g, inputChannel)
+
+	return g.score
+}
+
+// Run is the entrance of game 2048 in cmd
+func Run(name string, width int, height int, difficult int) {
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	inputChannel := make(chan int, 5)
+	go listenToInput(inputChannel)
+
+	game := Game{}
+
+	score := run(game, width, height, difficult, inputChannel, renderToScreen)
+	fmt.Println("your final socre is: ", score)
+}
